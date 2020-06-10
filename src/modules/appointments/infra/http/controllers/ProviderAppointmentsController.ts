02fbd4af4eb0512f8674dee0d217576a184c137a
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-06-10 20:16:21 -0300

    correcoes do projeto

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -5,7 +5,7 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
@@ -13,9 +13,9 @@ export default class ProviderDayAvailabilityController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
